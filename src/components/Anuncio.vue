89fd4d7d1271e7aefc90690b83abbaf980e8171e
Anibal <anibal.marquez.91@gmail.com>
2019-03-05 20:40:22 -0400

    First commit project
    Basic Vue project about reservation of seats in the cinema.

---
+++ src/components/Anuncio.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="anuncio">
+    <transition name="fade" mode="out-in">
+      <span :key="indice">
+        {{ presentaciones[indice] }}
+      </span>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      presentaciones: [
+        'Capitana Marvel - 20:30',
+        'Green Book - 15:00',
+        'El Vice Presidente - 16:00',
+        'El Rey León - 22:00'
+      ],
+      indice: 0
+    }
+  },
+  methods: {
+    mostrarAnuncio() {
+      this.indice++
+
+      if(this.indice == this.presentaciones.length) {
+        this.indice = 0
+        //Se recomienda nunca destruirlo por nosotros mismos, sino que vue lo destruya el mismo: this.$destroy()
+      }
+    }
+  },
+  mounted() {
+    setInterval(this.mostrarAnuncio, 3000)
+  }
+}
+</script>
+
+<style>
+.anuncio {
+  margin: 10px;
+  font-size: 2.2rem;
+
+  border-top-color: white;
+  border-top-width: 1px;
+  border-top-style: solid;
+
+  border-bottom-color: white;
+  border-bottom-width: 1px;
+  border-bottom-style: solid;
+}
+
+.fade-enter {
+  opacity: 0;
+}
+
+.fade-enter-active {
+  transition: opacity 1s ease-out;
+}
+
+.fade-leave-active {
+  transition: opacity .6s ease-out;
+  opacity: 0;
+}
+
+</style>
---
+++ src/components/Email.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="seccion">
+        <div class="atributo">
+            <!-- @ inicia el evento -->
+            <input v-model="email" @keyup.enter="actualizarEmail" type="text">
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['emailRecibido'],
+    data() {
+        return {
+            email: ''
+        }
+    },
+    methods: {
+        actualizarEmail(event) {
+            //Emit es Comunicacion hacia el padre
+            this.$emit('onCambioEmail', this.email)
+        }
+    },
+    watch: {
+        emailRecibido() {
+            this.email = this.emailRecibido
+        }
+    }
+}
+</script>
+
+<style>
+
+.email {
+    text-align: left;
+}
+
+</style>
+
